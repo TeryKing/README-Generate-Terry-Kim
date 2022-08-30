ef79b10730f6ab98341e2292a879324855f42b0d
TeryKing <tery_x3@hotmail.com>
2022-08-29 21:00:14 -0500

    writing the questions for the readme generator

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,59 @@
-// TODO: Include packages needed for this application
+// // TODO: Include packages needed for this application
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// // TODO: Create an array of questions for user input
+// const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to initialize app
+// function init() {}
 
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init();
+
+
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+inquirer
+  .prompt([
+    {
+        type : "input",
+        name: "title",
+        message: "What is the name of your project?",
+
+    },
+    {
+        type: "input",
+        name: "description",
+        message:"Briefly describe your project.",
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "If you have any installation process, add here."
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Pick any appropriate license for this project.",
+        choices: ["Apache", "GNU", "MIT"]
+    },
+    {
+        type: "input",
+        name: "contributors",
+        message: "Add your contributors for this project."
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter your email."
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "Please enter your username."
+    },
+  ])
+
