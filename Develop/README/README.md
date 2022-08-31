73aaa449f2cc48fb56e7a0da5f62521956a8d72e
TeryKing <tery_x3@hotmail.com>
2022-08-31 17:40:18 -0500

    added license

---
+++ Develop/README/README.md
@@ -0,0 +1,28 @@
+
+  #Project Title
+  example
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+# Description
+example
+# Table of Contents 
+* [Installation](#-Installation)
+* [Usage](#-Usage)
+* [License](#-Installation)
+* [Contributing](#-Contributing)
+* [Tests](#-Tests)
+* [Questions](#-Contact-Information)
+    
+# Installation
+example
+# Usage
+undefined
+# License 
+MIT
+* As this license list was not comprehensive, if you need another license, use the contact information below to ask for license to be added. 
+# Contributing 
+undefined
+# Tests
+undefined
+# Contact Information 
+* GitHub Username: undefined
+* Contact Email: undefined
--- Develop/index.js
+++ Develop/index.js
@@ -17,8 +17,7 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
-inquirer
-  .prompt([
+const questions = [
     {
         type : "input",
         name: "title",
@@ -39,7 +38,7 @@ inquirer
         type: "list",
         name: "license",
         message: "Pick any appropriate license for this project.",
-        choices: ["Apache", "GNU", "MIT"]
+        choices: ["Apache", "GNU", "MIT", "None"]
     },
     {
         type: "input",
@@ -57,21 +56,36 @@ inquirer
         message: "Please enter your username."
     },
   ]
-  )
+  
 
-// function writeToFile(fileName, data)
-// {
-//     fs.writeFile(fileName,generateMarkdown(data), function(err){
-//         if(err){
-//             return console.log(err);
-//         }
-//     });
-// }
+function writeToFile(fileName, data)
+{
+    fs.writeFile(fileName,generateMarkdown(data), function(err){
+        if(err){
+            return console.log(err);
+        }
+    });
+}
+function init(){
+    inquirer.prompt(questions).then((data) => {
+        console.log(JSON.stringify(data, null, " "));
+        data.getLicense = getLicense(data.license);
+        writeToFile("./README/README.md", data);
+    });
+}
 
-// function init(){
-//     const response = prompt(answers);
-//     const generateContent = generateReadme(response);
-//     fs.writeFile("./README/README.md",generateContent);
-// }
+function getLicense(value){
+    if (value === "GNU") {
+        return "[![License: GNU](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)";
+    } else if (value === "Apache") {
+        return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
 
-// init();
+    } else if (value === "MIT") {
+        return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+    }
+    else{
+        return "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)"
+    }
+}
+
+init();
